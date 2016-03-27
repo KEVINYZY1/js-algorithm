0d700775bbfb3f817c26ab7d632a2057f6689634
woniuppp <i@shengxinjing.cn>
2016-03-27 11:12:03 +0800

    二分查找

--- binarySearch.js
+++ binarySearch.js
@@ -2,8 +2,8 @@
 arr = [1,3,5,7,11,24,35,100]
 
 function binarySearch(arr,n){
-	var end = arr.length,start = 0
-	while(true){
+	var end = arr.length,start = 0,res
+	while(end>start){
 		var mid = (end+start)>>1
 		if (arr[mid]>n) {
 			end = mid-1
@@ -12,10 +12,15 @@ function binarySearch(arr,n){
 			start = mid+1
 		};
 		if (arr[mid]==n) {
-			console.log(mid)
+			res = mid
 			break
 		};
 
+	}
+	if (res) {
+		console.log(res)
+	}else{
+		console.log('not exist')
 	}
 }
 
