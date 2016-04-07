4b3d8fa0ce9324d7aeeaffaeb2732630a9d7c7c8
woniuppp <i@shengxinjing.cn>
2016-04-07 21:39:20 +0800

    数据

--- insertionSort.js
+++ insertionSort.js
@@ -1,7 +1,8 @@
 function insertion(arr){
-	for (var i = 0; i < arr.length; i++) {
+	var len = arr.length
+	for (var i = 0; i < len; i++) {
 		// var min = i
-		for (var j = i+1; j <arr.length; j++) {
+		for (var j = i+1; j <len; j++) {
 			if (arr[j]<arr[i]) {
 
 				var tmp = arr[i]
--- selectionSort.js
+++ selectionSort.js
@@ -1,7 +1,8 @@
 function selection(arr){
-	for (var i = 0; i < arr.length; i++) {
+	var len = arr.length
+	for (var i = 0; i < len; i++) {
 		var min = i
-		for (var j = i+1; j <arr.length; j++) {
+		for (var j = i+1; j <len; j++) {
 			if (arr[j]<arr[min]) {
 				min = j
 			};
